4d85753e50627ed100d49e516ee21e4178c4122a
Enrique Largacha Gil <107465968+QuiqueLargachaGil@users.noreply.github.com>
2023-06-07 22:31:10 +0200

    [FEATURE] Added Prism tabbed page

---
+++ src/MVP.PrismMaui/ViewModels/AboutViewModel.cs
@@ -0,0 +1,28 @@
+﻿using MVP.PrismMaui.Services.Abstractions;
+
+namespace MVP.PrismMaui.ViewModels
+{
+    public class AboutViewModel : BindableBase
+    {
+        private readonly IVersionService _versionService;
+
+        private string _version;
+
+        public AboutViewModel(IVersionService versionService)
+        {
+            _versionService = versionService;
+            LoadData();
+        }
+
+        public string Version
+        {
+            get => _version;
+            set => SetProperty(ref _version, value);
+        }
+
+        private void LoadData()
+        {
+            Version = _versionService.GetVersion();
+        }
+    }
+}
